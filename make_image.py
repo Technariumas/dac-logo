272d9519541849eb374f8df90950cc23ddb505b4
opit <opit@hardcore.lt>
2017-12-20 16:49:48 +0200

    fixed size

--- make_image.py
+++ make_image.py
@@ -1,7 +1,7 @@
 from __future__ import division
 import numpy as np
 from scipy import misc
-#import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 import sys
 
 #Usage: python make_image.py <input image> <output filename>
@@ -15,9 +15,9 @@ im = misc.imread(fname, flatten=True, mode='L')
 #Comment this line if your image should not be inverted, i.e. if white regions should be bright.
 im = 1 - im
 
-#plt.imshow(im, cmap='Greys')
-#plt.show()
+plt.imshow(im, cmap='Greys')
+plt.show()
 
-np.savetxt(output, np.transpose(np.nonzero(im)), fmt='%i', delimiter=",", newline="\n") 
+np.savetxt(output, np.transpose(np.where(im > 0)), fmt='%i', delimiter=",", newline="\n") 
 
 
